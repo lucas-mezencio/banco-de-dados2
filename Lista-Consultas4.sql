52520aad367083354f4792a9720c93b265a295f8
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-08 10:35:09 -0300

    lista 4 finalizada

--- Lista-Consultas4.sql
+++ Lista-Consultas4.sql
@@ -1,38 +1,48 @@
-﻿--01-Nome e cidade de clientes que possuem algum emprestimo, ordenado pelo nome do cliente
+﻿ --01-Nome e cidade de clientes que possuem algum emprestimo, ordenado pelo nome do cliente
 
-SELECT DISTINCT cliente.nome_cliente, cidade_cliente
-FROM emprestimo, 
-WHERE cliente.nome_cliente=nome_cliente
+-- produto cartesiano, pouco eficiente porque cria uma tabela com N tuplas 
+-- sendo N o produto das tuplas das duas tabelas
+SELECT DISTINCT cliente.nome_cliente, cidade_cliente 
+FROM emprestimo, cliente
+WHERE cliente.nome_cliente= emprestimo.nome_cliente
 ORDER BY cliente.nome_cliente;
+
+
+-- bem mais eficiente!!!
+--select distinct c.nome_cliente, cidade_cliente
+--from emprestimo e 
+--    inner join cliente c
+--    on c.nome_cliente = e.nome_cliente;
 
 --02-Nome e cidade de clientes que possuem emprestimo na PUC, ordenado pelo nome do cliente
-
-SELECT DISTINCT nome_cliente, cidade_cliente
-FROM cliente
-WHERE cliente.nome_cliente=nome_cliente
-and   nome_agencia='PUC'
-ORDER BY cliente.nome_cliente;
+ 
+SELECT DISTINCT cliente. nome_cliente, cidade_cliente
+FROM cliente, emprestimo
+WHERE cliente.nome_cliente = emprestimo.nome_cliente
+and emprestimo.nome_agencia='PUC'
+ORDER BY cliente.nome_cliente
 
 --03-Nomes de Clientes com saldo entre 100 e 900
 
-SELECT Cliente.nome_cliente
-FROM Cliente Deposito
-WHERE nome_cliente=nome_cliente
+select distinct Cliente.nome_cliente
+FROM Cliente, Deposito
+WHERE cliente.nome_cliente= deposito.nome_cliente
 AND Deposito.saldo_deposito between 100 and 900;
 
 --04-Nomes de clientes, valores de depósitos e empréstimos na agencia PUC
 SELECT D.nome_cliente, D.saldo_deposito, E.valor_emprestimo, E.nome_agencia
-FROM deposito, emprestimo AS E
-WHERE D.nome_agencia='PUC' AND
-=E.nome_agencia AND
-D.nome_cliente=
+FROM deposito  d, emprestimo AS E
+WHERE D.nome_agencia = 'PUC' 
+and d.nome_agencia = E.nome_agencia 
+and D.nome_cliente = e.nome_cliente
 
 ﻿--05-Selecione os nomes dos clientes da cidade de Contagem com depósitos maiores do que R$ 3.000,00
 
 select cliente.nome_cliente, cliente.cidade_cliente, deposito.saldo_deposito
 from cliente, deposito
 where  cliente.cidade_cliente = 'Contagem'
-and deposito.saldo_deposito > 3000;
+and deposito.saldo_deposito > 3000
+and cliente.nome_cliente = deposito.nome_cliente;
 
 --06-Um gerente pretende criar uma lista de clientes que correm o risco
 --de se individar de maneira irreversível. Para tanto ele formulou
@@ -42,8 +52,9 @@ and deposito.saldo_deposito > 3000;
 
 select cliente.nome_cliente, cliente.cidade_cliente, 
        deposito.saldo_deposito, emprestimo.valor_emprestimo
-from cliente, emprestimo
+from cliente, emprestimo, deposito
 where cliente.nome_cliente = deposito.nome_cliente
+and cliente.nome_cliente = emprestimo.nome_cliente 
 and cliente.cidade_cliente = 'Santa Luzia'
 and deposito.saldo_deposito < 1000
 and emprestimo.valor_emprestimo > 1000
@@ -55,12 +66,14 @@ and emprestimo.valor_emprestimo > 1000
 --Selecione os clientes da cidade de Santa Luzia com uma média de 
 --depósitos menor do que a média de empréstimos
 
-select cliente.nome_cliente, deposito.saldo_deposito, 
-       emprestimo.valor_emprestimo
+select cliente.nome_cliente, avg(deposito.saldo_deposito), 
+       avg(emprestimo.valor_emprestimo)
 from cliente, deposito, emprestimo
 where cliente.nome_cliente = deposito.nome_cliente
+and cliente.nome_cliente = emprestimo.nome_cliente
 and cliente.cidade_cliente = 'Santa Luzia'
-having avg(deposito.saldo_deposito) < emprestimo.valor_emprestimo
+group by cliente.nome_cliente
+having avg(deposito.saldo_deposito) < avg(emprestimo.valor_emprestimo)
 
 --08-É preciso atualizar a informação do saldo do cliente na tabela cliente.
 --para este propósito devemos levar em conta o saldo dos depósitos menos os
@@ -76,9 +89,9 @@ GROUP BY NUMERO_CONTA, NOME_AGENCIA, NOME_CLIENTE
 
 --Primeiro os clientes que possuem deposito e emprestimos (ambos)
 
-SELECT	E.NOME_CLIENTE, E.NOME_AGENCIA, E.NUMERO_CONTA,
-        SUM(D.SALDO_DEPOSITO), SUM(E.VALOR_EMPRESTIMO), 
-	SUM(D.SALDO_DEPOSITO)-SUM(E.VALOR_EMPRESTIMO) AS TOTAL
+select E.NOME_CLIENTE, E.NOME_AGENCIA, E.NUMERO_CONTA,
+        SUM(D.SALDO_DEPOSITO) as depositos, SUM(E.VALOR_EMPRESTIMO) as emprestimos, 
+        SUM(D.SALDO_DEPOSITO)-SUM(E.VALOR_EMPRESTIMO) AS TOTAL
 FROM EMPRESTIMO AS E, DEPOSITO AS D
 WHERE E.NOME_CLIENTE = D.NOME_CLIENTE
 GROUP BY E.NOME_CLIENTE, E.NOME_AGENCIA, E.NUMERO_CONTA
@@ -90,7 +103,7 @@ FROM(
 	SELECT	E.NOME_CLIENTE, E.NOME_AGENCIA, E.NUMERO_CONTA,
 		SUM(D.SALDO_DEPOSITO), SUM(E.VALOR_EMPRESTIMO), 
 		SUM(D.SALDO_DEPOSITO)-SUM(E.VALOR_EMPRESTIMO) AS TOTAL
-	FROM EMPRESTIMO AS E, DEPOSITO
+	FROM EMPRESTIMO AS E, DEPOSITO d
 	GROUP BY E.NOME_CLIENTE, E.NOME_AGENCIA, E.NUMERO_CONTA
 ) AS RELATORIO
 WHERE CONTA.NOME_CLIENTE = RELATORIO.NOME_CLIENTE
